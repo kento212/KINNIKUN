9eef0f2a417acb0ab58eb52cc98bf6e79a5bc1ec
kento212 <122858770+kento212@users.noreply.github.com>
2023-01-17 15:44:17 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+led.plot(0, 0)
+music.playTone(262, music.beat(BeatFraction.Whole))
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
